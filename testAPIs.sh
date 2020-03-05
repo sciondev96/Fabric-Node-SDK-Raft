0325395fdfea36ed54b97cd4016a95aaeb26d971
Satvik <49191921+sciondev96@users.noreply.github.com>
2020-03-05 13:05:08 +0530

    Update testAPIs.sh

--- testAPIs.sh
+++ testAPIs.sh
@@ -425,64 +425,64 @@ curl -s -X GET \
 echo
 echo
 
-# echo "GET query Block by blockNumber of chaincode 1"
-# echo
-# BLOCK_INFO=$(curl -s -X GET \
-#   "http://localhost:4000/channels/mychannel/blocks/1?peer=peer0.org1.example.com" \
-#   -H "authorization: Bearer $ORG1_TOKEN" \
-#   -H "content-type: application/json")
-# echo $BLOCK_INFO
-# # Assign previous block hash to HASH
-# HASH=$(echo $BLOCK_INFO | jq -r ".header.previous_hash")
-# echo
+echo "GET query Block by blockNumber of chaincode 1"
+echo
+BLOCK_INFO=$(curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/blocks/1?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json")
+echo $BLOCK_INFO
+# Assign previous block hash to HASH
+HASH=$(echo $BLOCK_INFO | jq -r ".header.previous_hash")
+echo
 
-# echo "GET query Block by blockNumber of chaincode 2"
-# echo
-# BLOCK_INFO=$(curl -s -X GET \
-#   "http://localhost:4000/channels/mychannel/blocks/5?peer=peer0.org1.example.com" \
-#   -H "authorization: Bearer $ORG1_TOKEN" \
-#   -H "content-type: application/json")
-# echo $BLOCK_INFO
-# # Assign previous block hash to HASH
-# HASH=$(echo $BLOCK_INFO | jq -r ".header.previous_hash")
-# echo
+echo "GET query Block by blockNumber of chaincode 2"
+echo
+BLOCK_INFO=$(curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/blocks/5?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json")
+echo $BLOCK_INFO
+# Assign previous block hash to HASH
+HASH=$(echo $BLOCK_INFO | jq -r ".header.previous_hash")
+echo
 
-# echo "GET query Transaction by TransactionID 1"
-# echo
-# curl -s -X GET http://localhost:4000/channels/mychannel/transactions/$TRX_ID?peer=peer0.org1.example.com \
-#   -H "authorization: Bearer $ORG1_TOKEN" \
-#   -H "content-type: application/json"
-# echo
-# echo
+echo "GET query Transaction by TransactionID 1"
+echo
+curl -s -X GET http://localhost:4000/channels/mychannel/transactions/$TRX_ID?peer=peer0.org1.example.com \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
 
-# echo "GET query Transaction by TransactionID 2"
-# echo
-# curl -s -X GET http://localhost:4000/channels/mychannel/transactions/$TRX_ID1?peer=peer0.org1.example.com \
-#   -H "authorization: Bearer $ORG1_TOKEN" \
-#   -H "content-type: application/json"
-# echo
-# echo
+echo "GET query Transaction by TransactionID 2"
+echo
+curl -s -X GET http://localhost:4000/channels/mychannel/transactions/$TRX_ID1?peer=peer0.org1.example.com \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
 
 
-# echo "GET query Block by Hash - Hash is $HASH"
-# echo
-# curl -s -X GET \
-#   "http://localhost:4000/channels/mychannel/blocks?hash=$HASH&peer=peer0.org1.example.com" \
-#   -H "authorization: Bearer $ORG1_TOKEN" \
-#   -H "cache-control: no-cache" \
-#   -H "content-type: application/json" \
-#   -H "x-access-token: $ORG1_TOKEN"
-# echo
-# echo
+echo "GET query Block by Hash - Hash is $HASH"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel/blocks?hash=$HASH&peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -H "x-access-token: $ORG1_TOKEN"
+echo
+echo
 
-# echo "GET query ChainInfo"
-# echo
-# curl -s -X GET \
-#   "http://localhost:4000/channels/mychannel?peer=peer0.org1.example.com" \
-#   -H "authorization: Bearer $ORG1_TOKEN" \
-#   -H "content-type: application/json"
-# echo
-# echo
+echo "GET query ChainInfo"
+echo
+curl -s -X GET \
+  "http://localhost:4000/channels/mychannel?peer=peer0.org1.example.com" \
+  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "content-type: application/json"
+echo
+echo
 
 echo "GET query Installed chaincodes"
 echo
